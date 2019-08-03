90cb777df132ca882499061feb6ff611788cd8ab
Edward S. Vinyard <ed@tzahk.com>
2019-08-02 21:53:31 -0500

    switch from println() to SLF4J logging

--- ServiceThatLogs/src/main/java/com/tzahk/Application.java
+++ ServiceThatLogs/src/main/java/com/tzahk/Application.java
@@ -1,15 +1,18 @@
 package com.tzahk;
 
 import java.util.Arrays;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class Application {
+
+    final static Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -18,13 +21,17 @@ public class Application {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            System.out.println("Beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+            logBeanDefinitionNames(ctx);
         };
+    }
+
+    private static void logBeanDefinitionNames(ApplicationContext ctx) {
+        log.trace("Beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            log.trace(beanName);
+        }
     }
 }
--- ServiceThatLogs/src/main/java/com/tzahk/Controller.java
+++ ServiceThatLogs/src/main/java/com/tzahk/Controller.java
@@ -1,13 +1,26 @@
 package com.tzahk;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.google.common.base.Strings;
 
 @RestController
 public class Controller {
+    final Logger log = LoggerFactory.getLogger(Application.class);
 
-    @RequestMapping("/hello")
-    public String index() {
-        return "Hello, World!";
+    @GetMapping("/")
+    public String index(
+        @RequestHeader(name="User-Agent", required=false) final String userAgent)
+    {
+        log.info("request from " + userAgent);
+
+        if (Strings.isNullOrEmpty(userAgent)) {
+            return "Hello, stranger!";
+        } else {
+            return "Hello, " + userAgent + "!";
+        }
     }
 }
