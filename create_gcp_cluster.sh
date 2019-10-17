4ef8c4c7bf0e166ad39c02594bb6a1e93cbeeb77
Edward S. Vinyard <ed@tzahk.com>
2019-10-17 13:32:01 -0500

    update cluster create script with new K8s version and Stackdriver options

--- create_gcp_cluster.sh
+++ create_gcp_cluster.sh
@@ -2,15 +2,15 @@ gcloud beta container \
         --project "maximal-copilot-249415" clusters create "test" \
         --zone "us-central1-a" \
         --no-enable-basic-auth \
-        --cluster-version "1.12.8-gke.10" \
+        --cluster-version "1.13.7-gke.24" \
         --machine-type "n1-standard-1" \
         --image-type "COS" \
         --disk-type "pd-standard" \
         --disk-size "100" \
         --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
         --num-nodes "3" \
-        --enable-cloud-logging \
-        --enable-cloud-monitoring \
+        --enable-stackdriver-kubernetes \
+        --enable-stackdriver-kubernetes \
         --enable-ip-alias \
         --network "projects/maximal-copilot-249415/global/networks/default" \
         --subnetwork "projects/maximal-copilot-249415/regions/us-central1/subnetworks/default" \
